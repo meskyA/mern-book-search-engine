ac8ed07b32f541366783531c2f487526f564445f
meskyA <mesky2015@gmail.com>
2022-11-12 18:07:31 -0800

    adds strings in typedefs file for graphql

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+
+const typeDefs = gql`
+type Book {
+    _id: ID
+    title: String
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    forSale: String
+    link: String
+
+}
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Books] 
+    
+}
+{
+    typyr Query {
+        me: User
+    }
+}
+type Auth {
+    token: ID!
+    user: User
+}
+input SavedBookInput {
+    title: String
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    forSale: String
+    link: String
+}
+type Mutation {
+    login(email: String! password: String!): Auth 
+    addUser(username: String!, email: String!, password: String!); Auth
+    saveBook(book: SavedBookInput): User
+    removeBook(bookId: String!): User
+}`;
+
+module.exports = typeDefs;
