0c8b7c0248ac3c9fcebf0689bbf351269a4853e2
meskyA <mesky2015@gmail.com>
2022-11-12 16:26:17 -0800

    works in mutation and queries files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String) {
+    longin(email: $email, password: $password) {
+      token
+      user {
+        _ie
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($book: SavedBookInput!) {
+    saveBook(book: $book) {
+        username
+        emailbookCount
+        savedBooks {
+            title
+            authors
+            description
+            bookId
+            image
+            link
+        }
+    }
+}`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        username
+        email
+        bookCount
+        savedBooks {
+            title
+            authors
+            description
+            bookId
+            image
+            link
+        }
+    }
+}`;
