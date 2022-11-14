179dc392bf75496bb1a72d31494a25ebb93ed725
meskyA <mesky2015@gmail.com>
2022-11-14 14:47:58 -0800

    changed import in queres.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,7 @@
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-query me {
+{
     me {
         _id
         username
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('');
+const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
