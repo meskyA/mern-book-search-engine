ccc15da99bde7536664c647fd5cea8433a0fa60b
meskyA <mesky2015@gmail.com>
2022-11-18 18:39:04 -0800

    changes database name to match mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engine.books', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/book-search-engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
